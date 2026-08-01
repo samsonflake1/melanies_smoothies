96f468dc2c6dc30d340106ec5ba5b77f4396b092
Samson <116674038+samsonflake1@users.noreply.github.com>
2026-08-02 03:19:12 +0530

    Update streamlit_app.py
    container width changes - width='stretch'

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, width='stretch')
 # st.stop()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC functions
@@ -45,7 +45,7 @@ if ingredients_list:
 
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
-        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), width='stretch')
 
     # st.write(ingredients_string)
 
